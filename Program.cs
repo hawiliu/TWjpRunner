40d705a755f9b53f77f9b4074bde088800814235
hawiliu <hawiliu@gmail.com>
2020-10-14 22:42:26 +0800

    [main] Initial Commit

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+
+namespace TWjpRunner
+{
+    static class Program
+    {
+        static string NgmPath = @"C:\ProgramData\Nexon\NGM\NGM.exe";
+        static string NgmCommandLine = "";
+        static string TwCommandLine = "";
+        static string LEProcPath = @"";
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                //取得設定檔
+                IConfiguration config = new ConfigurationBuilder()
+                            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                            .Build();
+
+                var section = config.GetSection("TWjpRunner");
+
+                NgmPath = config.GetValue<string>("NgmPath", @"C:\ProgramData\Nexon\NGM\NGM.exe");
+                LEProcPath = config.GetValue<string>("LEProcPath", @"");
+
+                if (LEProcPath == "")
+                {
+                    Console.WriteLine("找不到LEProc...");
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    return;
+                }
+
+                //取得Web的指令
+                NgmCommandLine = args[0];
+
+                //執行NGM
+                Process.Start(new ProcessStartInfo()
+                {
+                    UseShellExecute = true,
+                    Verb = "runas",
+                    Arguments = $"{NgmCommandLine}",
+                    FileName = NgmPath
+                });
+
+                //等待TalesWeaver.exe執行並取得CommandLine
+                while (true)
+                {
+                    var Processes = Process.GetProcesses();
+                    var tw = Processes.Where(p => p.ProcessName.Equals("TalesWeaver")).FirstOrDefault();
+
+                    if (tw != null)
+                    {
+                        TwCommandLine = GetCommandLine(tw);
+
+                        tw.Kill();
+                        break;
+                    }
+                }
+
+                Process.Start(new ProcessStartInfo()
+                {
+                    UseShellExecute = true,
+                    Verb = "runas",
+                    Arguments = $"-run {TwCommandLine}",
+                    FileName = LEProcPath
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private static string GetCommandLine(this Process process)
+        {
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT CommandLine FROM Win32_Process WHERE ProcessId = " + process.Id))
+            using (ManagementObjectCollection objects = searcher.Get())
+            {
+                return objects.Cast<ManagementBaseObject>().SingleOrDefault()?["CommandLine"]?.ToString();
+            }
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# TWjpRunner
+一個不用修改系統非Unicode語言，卻又可以幫忙轉語系的小工具
+
+### **使用環境**
+* [Firefox](https://www.mozilla.org/zh-TW/firefox/new/)
+* [Locale.Emulator.2.4.1.0](https://xupefei.github.io/Locale-Emulator/)
+* [.Net Core 3.1 Runtime](https://dotnet.microsoft.com/download/dotnet-core/current/runtime)(必須安裝)
+
+### **下載**
+* [TWjpRunner](https://github.com/hawiliu/Upload/blob/master/TWjpRunner/TWjpRunner.zip?raw=true)
+
+### **使用教學**
+1. 下載TWjpRunner並放到任意位置(建議路徑內不要有中文名稱)
+2. 修改TWjpRunner資料夾內的appsetting.json的LEProcPath  
+依照填入Locale.Emulator放的位置自行填入LEProc.exe的路徑  
+理論上NgmPath不需要修改  
+![appsetting.json](https://raw.githubusercontent.com/hawiliu/Upload/master/TWjpRunner/Image/appsetting.PNG?token=AFPAKAAT6NTPBDI54QFLOCS7Q4FXQ)
+3. 開啟Firefox的選項，往下拉找到應用程式的ngmj選項，並修改成使用TWjpRunner.exe執行  
+![ngmj](https://raw.githubusercontent.com/hawiliu/Upload/master/TWjpRunner/Image/ngmj.PNG?token=AFPAKADGK3XLT5FNJ2FMP2C7Q4FOK)
+4. 正常在官網登錄並點GameStart
+5. 如果跳出是否允許執行請按是  
+![yes](https://raw.githubusercontent.com/hawiliu/Upload/master/TWjpRunner/Image/%E5%85%81%E8%A8%B1%E5%9F%B7%E8%A1%8C.PNG?token=AFPAKAGYCNE7Z7SSPZ2WGNK7Q4FZU)
+6. 如果是第一次執行可能會跳出LEGUI設定畫面，可參考下圖設定即可  
+![LEGUI](https://raw.githubusercontent.com/hawiliu/Upload/master/TWjpRunner/Image/LEGUI.PNG?token=AFPAKADQRNCRIMUXXSJGXY27Q4FSI)
+7. 開始玩遊戲囉~
+
+**聲明**  
+如果不當使用造成任何問題，後果請自行負責。
